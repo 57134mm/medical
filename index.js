e500bae0526f21f2f1e773d24ea6bf80f62f88e5
57134mm <Mansi.Mahant@merai.co>
2024-08-26 09:56:53 +0530

    add department

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const routerAuth = require('../medical/src/routes/auth');
 const routerPatient = require('../medical/src/routes/patient');
+const routerDepartment = require('../medical/src/routes/department');
 const swaggerUi = require('swagger-ui-express')
 const swaggerFile = require('./swagger-output.json')
 
@@ -13,10 +14,11 @@ app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerFile))
 
 app.use('/', routerAuth);
 app.use('/patient', routerPatient);
+app.use('/department', routerDepartment);
 
 const db = require('../medical/src/config/db');
 
-db.sync()
+db.sync({ force: false })
     .then(() => {
         console.log('Database connected successfully');
     })
